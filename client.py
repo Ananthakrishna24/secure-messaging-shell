5c9a002c77070f57daa68cbc2d7de0f3ae45533e
AnanthaKrishna <ananthaak24@gmail.com>
2023-10-28 20:29:07 +0530

    Major Changes to server and client

--- client.py
+++ client.py
@@ -1,38 +1,52 @@
 import socket
-from crypto_utils import generate_keypair, derive_shared_key, encrypt, decrypt
 from Crypto.PublicKey import ECC
+from crypto_utils import generate_keypair, derive_shared_key, encrypt, decrypt
 
 def start_client():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('127.0.0.1', 12345))
-        
-        # Generate and send public key
-        private_key, public_key = generate_keypair()
-        s.sendall(public_key.export_key(format="DER"))
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect(('127.0.0.1', 12345))
 
-        # Receive server's public key and derive shared key
-        server_public_key_data = s.recv(1024)
-        server_public_key = ECC.import_key(server_public_key_data)
-        shared_key = derive_shared_key(private_key, server_public_key)
+                # Generate and send public key
+                private_key, public_key = generate_keypair()
+                s.sendall(public_key.export_key(format="DER"))
 
-        # Messaging loop
-        while True:
-            message = input("Client: ")
-            nonce, ciphertext, tag = encrypt(shared_key, message)
-            enc_data = len(nonce).to_bytes(1, 'big') + nonce + len(ciphertext).to_bytes(1, 'big') + ciphertext + tag
-            s.sendall(enc_data)
+                # Receive server's public key and derive shared key
+                server_public_key_data = s.recv(1024)
+                server_public_key = ECC.import_key(server_public_key_data)
+                shared_key = derive_shared_key(private_key, server_public_key)
 
-            # Client waits for a message from the server
-            enc_data = s.recv(1024)
-            nonce_length = enc_data[0]
-            nonce = enc_data[1:1+nonce_length]
-            ciphertext_length = enc_data[1+nonce_length]
-            ciphertext = enc_data[2+nonce_length:2+nonce_length+ciphertext_length]
-            tag = enc_data[2+nonce_length+ciphertext_length:]
-            server_message = decrypt(shared_key, nonce, ciphertext, tag)
-            print(f"Received from server: {server_message}")
+                # Messaging loop
+                while True:
+                    message = input("Client: ")
+                    nonce, ciphertext, tag = encrypt(shared_key, message)
+                    enc_data = len(nonce).to_bytes(1, 'big') + nonce + len(ciphertext).to_bytes(1, 'big') + ciphertext + tag
+                    s.sendall(enc_data)
 
+                    enc_data = s.recv(1024)
+                    if not enc_data:
+                        raise ConnectionAbortedError("Server disconnected")
 
+                    nonce_length = enc_data[0]
+                    nonce = enc_data[1:1+nonce_length]
+                    ciphertext_length = enc_data[1+nonce_length]
+                    ciphertext = enc_data[2+nonce_length:2+nonce_length+ciphertext_length]
+                    tag = enc_data[2+nonce_length+ciphertext_length:]
+                    server_message = decrypt(shared_key, nonce, ciphertext, tag)
+                    print(f"Received from server: {server_message}")
+
+        except ConnectionAbortedError:
+            print("Primary Server for Chat Got Disconnected due to unknown reason, you might have to restart it")
+            break
+
+        except Exception as ex:
+            if ex.errno == 61:  # Connection refused error
+                print("Server unavailable. Switching to server mode...")
+                # Logic to switch client to server mode here
+            else:
+                print(f"An unexpected error occurred: {ex}")
+                break
 
 if __name__ == "__main__":
     start_client()
--- server.py
+++ server.py
@@ -2,42 +2,53 @@ import socket
 from Crypto.PublicKey import ECC
 from crypto_utils import generate_keypair, derive_shared_key, encrypt, decrypt
 
+HANDOVER_MESSAGE = "HANDOVER_SERVER_ROLE"
+
 def start_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('0.0.0.0', 12345))
-        s.listen()
-        print('Waiting for connection...')
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind(('0.0.0.0', 12345))
+                s.listen()
+                print('Waiting for connection...')
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected by', addr)
 
-            # Generate and send public key
-            private_key, public_key = generate_keypair()
-            conn.sendall(public_key.export_key(format="DER"))
+                    # Generate and send public key
+                    private_key, public_key = generate_keypair()
+                    conn.sendall(public_key.export_key(format="DER"))
 
-            # Receive client's public key and derive shared key
-            client_public_key_data = conn.recv(1024)
-            client_public_key = ECC.import_key(client_public_key_data)
-            shared_key = derive_shared_key(private_key, client_public_key)
+                    # Receive client's public key and derive shared key
+                    client_public_key_data = conn.recv(1024)
+                    client_public_key = ECC.import_key(client_public_key_data)
+                    shared_key = derive_shared_key(private_key, client_public_key)
 
-            # Messaging loop
-            while True:
-                enc_data = conn.recv(1024)
-                nonce_length = enc_data[0]
-                nonce = enc_data[1:1+nonce_length]
-                ciphertext_length = enc_data[1+nonce_length]
-                ciphertext = enc_data[2+nonce_length:2+nonce_length+ciphertext_length]
-                tag = enc_data[2+nonce_length+ciphertext_length:]
-                message = decrypt(shared_key, nonce, ciphertext, tag)
-                print(f"Received: {message}")
+                    # Messaging loop
+                    while True:
+                        server_message = input("Server: ")
+                        nonce, ciphertext, tag = encrypt(shared_key, server_message)
+                        enc_data = len(nonce).to_bytes(1, 'big') + nonce + len(ciphertext).to_bytes(1, 'big') + ciphertext + tag
+                        conn.sendall(enc_data)
 
-                # Server's turn to send a message
-                server_message = input("Server: ")
-                nonce, ciphertext, tag = encrypt(shared_key, server_message)
-                enc_data = len(nonce).to_bytes(1, 'big') + nonce + len(ciphertext).to_bytes(1, 'big') + ciphertext + tag
-                conn.sendall(enc_data)
+                        enc_data = conn.recv(1024)
+                        if not enc_data:
+                            raise ConnectionAbortedError("Client disconnected")
 
+                        nonce_length = enc_data[0]
+                        nonce = enc_data[1:1+nonce_length]
+                        ciphertext_length = enc_data[1+nonce_length]
+                        ciphertext = enc_data[2+nonce_length:2+nonce_length+ciphertext_length]
+                        tag = enc_data[2+nonce_length+ciphertext_length:]
+                        message = decrypt(shared_key, nonce, ciphertext, tag)
+                        print(f"Received: {message}")
 
+        except KeyboardInterrupt:
+            print("\nServer terminated by user.")
+            break
+
+        except Exception as ex:
+            print(f"An error occurred: {ex}")
 
 if __name__ == "__main__":
     start_server()
